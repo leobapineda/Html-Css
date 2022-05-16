ba848fdefda0146a3585b702ed6f8bb5613a870d
Leobardo Pineda <leobapineda@gmail.com>
2022-05-16 17:38:12 -0500

    Test: add translate property to send message btn v1
    Using the css translate and js calculate the correct units to move the message button from center top to bottom right# Please enter the commit message for your changes. Lines starting

---
+++ js/App.js
@@ -0,0 +1,36 @@
+let button = document.querySelector(".button");
+let buttonPosition = button.getBoundingClientRect();
+// distancia al borde superior del viewport
+console.log('top ' + buttonPosition.top);
+// distancia al borde derecho del viewport
+console.log('right ' + buttonPosition.right);
+// distancia al borde inferior del viewport
+console.log('bottom ' + buttonPosition.bottom);
+console.log('width ' + buttonPosition.width);
+
+console.log('left ' + buttonPosition.left);
+console.log(buttonPosition);
+
+let w = window.innerWidth
+let h = window.innerHeight
+
+console.log(`screenW ${w}`);
+console.log(`screenH ${h}`);
+
+1048
+625
+
+// la altura es 812.
+// el bottom esta a 84.5
+// restamos y sumamos 10
+
+button.addEventListener('click', () => {
+    // let www = (buttonPosition.left + (w/2) - (buttonPosition.width/2) - 10)/2
+    let www = (w/2) - (buttonPosition.width/2) - 10
+    let hhh = ((buttonPosition.top - h)*(-1)) + 10
+    
+    console.log(www);
+    document.documentElement.style.setProperty('--custom-w', `${www}px`)
+    document.documentElement.style.setProperty('--custom-h', `${hhh}px`)
+    console.log(hhh);
+})
