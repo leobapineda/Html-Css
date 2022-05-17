ed957c835403812683024d32ff1630b004cc276e
Leobardo Pineda <leobapineda@gmail.com>
2022-05-16 21:30:51 -0500

    Feat: add message button animation on scroll down

--- js/App.js
+++ js/App.js
@@ -1,31 +1,36 @@
 let button = document.querySelector(".button");
 let buttonPosition = button.getBoundingClientRect();
-// distancia al borde superior del viewport
-console.log('top ' + buttonPosition.top);
-// distancia al borde derecho del viewport
-console.log('right ' + buttonPosition.right);
-// distancia al borde inferior del viewport
-console.log('bottom ' + buttonPosition.bottom);
-console.log('left ' + buttonPosition.left);
-console.log('width ' + buttonPosition.width);
-console.log('height ' + buttonPosition.height);
+let w = window.innerWidth;
+let h = window.innerHeight;
+
+button.addEventListener("click", () => {
+  // let www = (w/2) - (buttonPosition.width/2) - 10
+  
+});
+
+const nav = document.querySelector(".nav");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav--hidden");
+    button.classList.add('button__down')
+    let www = w / 2 - buttonPosition.width / 2;
+  // let hhh = ((h - buttonPosition.top) - buttonPosition.height - 30)
+  let hhh = h - buttonPosition.top - buttonPosition.width;
+  document.documentElement.style.setProperty("--custom-w", `${www}px`);
+  document.documentElement.style.setProperty("--custom-h", `${hhh}px`);
 
 
-let w = window.innerWidth
-let h = window.innerHeight
 
-console.log(`screenW ${w}`);
-console.log(`screenH ${h}`);
+  } else {
+    nav.classList.remove("nav--hidden");
+    button.classList.remove('button__down')
+
+  }
+
+  lastScrollY = window.scrollY;
+});
 
 
-button.addEventListener('click', () => {
-    // let www = (w/2) - (buttonPosition.width/2) - 10
-    let www = (w/2)- (buttonPosition.width/2)
-    // let hhh = ((h - buttonPosition.top) - buttonPosition.height - 30)
-    let hhh = (h - buttonPosition.top) - buttonPosition.width
-    // console.log(www);
-    document.documentElement.style.setProperty('--custom-w', `${www}px`)
-    document.documentElement.style.setProperty('--custom-h', `${hhh}px`)
-    console.log(hhh);
-
-})
+// tengo que cambiar como se ejecuta. ahora se ejecuta con un focus. cambialo agregando una clase
