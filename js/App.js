e66841df23e68a929f332e6e680c8ff46aae5d77
Leobardo Pineda <leobapineda@gmail.com>
2022-05-17 10:45:06 -0500

    Feat: add message btn transition on scroll v1

--- js/App.js
+++ js/App.js
@@ -1,36 +1,22 @@
 let button = document.querySelector(".button");
-let buttonPosition = button.getBoundingClientRect();
-let w = window.innerWidth;
-let h = window.innerHeight;
-
-button.addEventListener("click", () => {
-  // let www = (w/2) - (buttonPosition.width/2) - 10
-  
-});
 
 const nav = document.querySelector(".nav");
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
-  if (lastScrollY < window.scrollY) {
-    nav.classList.add("nav--hidden");
-    button.classList.add('button__down')
-    let www = w / 2 - buttonPosition.width / 2;
-  // let hhh = ((h - buttonPosition.top) - buttonPosition.height - 30)
-  let hhh = h - buttonPosition.top - buttonPosition.width;
-  document.documentElement.style.setProperty("--custom-w", `${www}px`);
-  document.documentElement.style.setProperty("--custom-h", `${hhh}px`);
-
-
-
-  } else {
-    nav.classList.remove("nav--hidden");
-    button.classList.remove('button__down')
-
+  if (window.scrollY >= 20) {
+    button.classList.add("button__down");
   }
 
+  if (lastScrollY < window.scrollY && window.scrollY >= 35) {
+    nav.classList.add("nav--hidden");
+    
+  } else {
+    nav.classList.remove("nav--hidden");
+    if(window.scrollY <= 30){
+      button.classList.remove("button__down");
+    }
+  }
   lastScrollY = window.scrollY;
+  console.log( window.scrollY);
 });
-
-
-// tengo que cambiar como se ejecuta. ahora se ejecuta con un focus. cambialo agregando una clase
