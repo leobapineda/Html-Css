bbba4122f91a9d7facd1cb6f88397430b6660b2e
Leobardo Pineda <leobapineda@gmail.com>
2022-05-16 20:49:31 -0500

    Test: finish message btn tranlate animation
    It is using the dimensions of the square btn, not the last circular btn

--- js/App.js
+++ js/App.js
@@ -6,11 +6,10 @@ console.log('top ' + buttonPosition.top);
 console.log('right ' + buttonPosition.right);
 // distancia al borde inferior del viewport
 console.log('bottom ' + buttonPosition.bottom);
+console.log('left ' + buttonPosition.left);
 console.log('width ' + buttonPosition.width);
 console.log('height ' + buttonPosition.height);
 
-console.log('left ' + buttonPosition.left);
-console.log(buttonPosition);
 
 let w = window.innerWidth
 let h = window.innerHeight
@@ -18,20 +17,15 @@ let h = window.innerHeight
 console.log(`screenW ${w}`);
 console.log(`screenH ${h}`);
 
-1048
-625
-
-// la altura es 812.
-// el bottom esta a 84.5
-// restamos y sumamos 10
 
 button.addEventListener('click', () => {
-    // let www = (buttonPosition.left + (w/2) - (buttonPosition.width/2) - 10)/2
-    let www = (w/2) - (buttonPosition.width/2) - 10
-    let hhh = ((buttonPosition.top - h)*(-1) - buttonPosition.height - 30)
-    
-    console.log(www);
+    // let www = (w/2) - (buttonPosition.width/2) - 10
+    let www = (w/2)- (buttonPosition.width/2)
+    // let hhh = ((h - buttonPosition.top) - buttonPosition.height - 30)
+    let hhh = (h - buttonPosition.top) - buttonPosition.width
+    // console.log(www);
     document.documentElement.style.setProperty('--custom-w', `${www}px`)
     document.documentElement.style.setProperty('--custom-h', `${hhh}px`)
     console.log(hhh);
+
 })
