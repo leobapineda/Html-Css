08f81c088931fbe86085391f2b42c4d2b1baca35
Leobardo Pineda <leobapineda@gmail.com>
2022-05-17 22:14:01 -0500

    Refactor: remove unnecessary googlebug.js and better html syntax

--- js/App.js
+++ js/App.js
@@ -10,19 +10,12 @@ window.addEventListener("scroll", () => {
 
   if (lastScrollY < window.scrollY && window.scrollY >= 35) {
     nav.classList.add("nav--hidden");
-    
   } else {
     nav.classList.remove("nav--hidden");
-    if(window.scrollY <= 30){
+    if (window.scrollY <= 30) {
       button.classList.remove("button__down");
     }
   }
   lastScrollY = window.scrollY;
-  console.log( window.scrollY);
+  console.log(window.scrollY);
 });
-
-
-// necesito que te quedes ahi
-
-// tengo que hacer un div que cubra todo el primer vh
-// 
